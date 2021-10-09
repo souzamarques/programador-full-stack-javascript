93e40ee5faf5697d817793588ef7d0344e56d0b0
Lucas Souza <lucassouzzamarques@gmail.com>
2021-10-09 08:32:15 -0300

    JavaScript + Lógica de Programação - Aula 01: Tipos Primitivos

---
+++ Módulo 6 - JavaScript + Logica/exemplos/01TiposPrimitivos.js
@@ -0,0 +1,18 @@
+/* Tipo number: utilizado para fazer uma representação numérica.
+   Todos os numeros estão incluidos nesse tipo, seja inteiros ou decimais. */
+
+console.log("Testando:", 5)
+console.log("Testano novamente:", 2 * 3.5)
+
+/* Tipo string: cadeia de caracteres */
+console.log("Spaceship")
+console.log('Spaceship em aspas simples')
+
+/* Tipo booleano: assume apenas dois valores: true e false */
+console.log("5 > 10:", 5 > 10)
+console.log("100 > 10:", 100 > 10)
+
+/* Tipo null e undefined:
+   null: representa valor nulo ou vazio.
+   undefined: interpretar que é um valor indefinido. Utilizado quando a linguagem
+              não consegue definir um tipo. */
