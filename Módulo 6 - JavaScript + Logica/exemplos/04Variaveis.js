8cc4b02e24d03cecfbc42df45d090426b69835d1
Lucas Souza <lucassouzzamarques@gmail.com>
2021-10-10 13:04:21 -0300

    Declarando Variáveis

---
+++ Módulo 6 - JavaScript + Logica/exemplos/04Variaveis.js
@@ -0,0 +1,32 @@
+// Declaração de Variável com let
+let spaceship = "FE Supernova"
+console.log(spaceship)  
+spaceship = "FE Helmet"
+console.log(spaceship)
+
+// Declaração de Variável com const (Constante)
+const squad = "Estelar"
+console.log(squad)
+/* squad = "Espacial"
+console.log(squad)   Não é possível atribuir um novo valor a uma variável constante */
+
+// Declaração de Variável com var (Funciona como o let)
+var velocidade = 100
+console.log(velocidade)
+velocidade = 120
+console.log(velocidade)
+velocidade = "ETS"
+console.log(velocidade)
+
+let nulo
+console.log(nulo)
+
+/*
+let depConfirmacao = confirm("Pronto para dirigir. Confirmar ?")
+console.log(depConfirmacao)
+OBS: aparecerá uma tela, e caso seja clicado o botao "OK", aparecerá a mensagem true.
+     Caso "Cancelar/Cancel" seja clicado, aparecerá a mensagem false.
+--------------------------------------------------------------------------------------
+let motorista = prompt("Caminhoneiro(a), qual é o seu nome ?")
+console.log(motorista)
+*/
