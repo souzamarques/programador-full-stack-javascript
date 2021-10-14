611f339175792d103962c77489fdb202743075fa
Lucas Souza <lucassouzzamarques@gmail.com>
2021-10-14 11:14:39 -0300

    Condicionais

---
+++ Módulo 6 - JavaScript + Logica/exemplos/08Condicional.js
@@ -0,0 +1,9 @@
+let velocidade = 80
+
+if(velocidade < 100) {
+    console.log("Velocidade aceitável")
+} else {
+    console.log("Velocidade de risco")
+}
+
+(velocidade > 100) ? console.log("Acima de 100km/h") : console.log("Abaixo de 100km/h")
