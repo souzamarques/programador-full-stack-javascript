039e5bfe74657c17fac209109a36ab8dda89299a
Lucas Souza <lucassouzzamarques@gmail.com>
2021-10-14 00:33:53 -0300

    Operações Lógicas

---
+++ Módulo 6 - JavaScript + Logica/exemplos/07logica.js
@@ -0,0 +1,9 @@
+let spaceship = "Elemental"
+let velocidade = 20
+
+console.log((spaceship.length == 9) && (velocidade > 15))
+console.log((spaceship.length == 9) && (velocidade > 30))
+
+console.log((spaceship.length == 20) || (velocidade > 15))
+console.log(!(velocidade == 20))
+console.log(!(velocidade > 30))
