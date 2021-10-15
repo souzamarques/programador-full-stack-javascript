ab09403eaab6cfab0fe8c6825df8b34694e842fd
Lucas Souza <lucassouzzamarques@gmail.com>
2021-10-15 01:16:32 -0300

    Switch

---
+++ Módulo 6 - JavaScript + Logica/exemplos/10Switch.js
@@ -0,0 +1,12 @@
+let velocidade = 80
+
+switch(velocidade * 2) {
+    case 100:
+        console.log("Sua velocidade é 100km/s")
+        break;
+    case 160:
+        console.log("Sua velocidade é 160km/s")
+        break;
+    default:
+        console.log("Velocidade não identificada")
+}
