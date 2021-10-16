9292b129db199ac1037f4ec76edb0fe224a6d75f
Lucas Souza <lucassouzzamarques@gmail.com>
2021-10-16 11:57:58 -0300

    Parâmetros das Funções

--- Módulo 6 - JavaScript + Logica/exemplos/13Function.js
+++ Módulo 6 - JavaScript + Logica/exemplos/13Function.js
@@ -1,5 +1,15 @@
 function greetPilot() {
-    alert("Bom dia, piloto!")
+    console.log("Bom dia, piloto!")
+}
+
+function speedUp(velocidade, aceleracao) {
+    let novaVelocidade = velocidade + aceleracao
+    console.log("Acelerando. Nova velocidade: " + novaVelocidade + "km/s")
 }
 
 greetPilot()
+
+let velocidade = 50
+let aceleracao = 30
+console.log("Velocidade: " + velocidade + "km/s")
+speedUp(velocidade, aceleracao)
