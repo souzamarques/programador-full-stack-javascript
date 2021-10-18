2e7e8803a6c87a190b2ea487d37f430a68cbbaec
Lucas Souza <lucassouzzamarques@gmail.com>
2021-10-18 15:47:04 -0300

    Introdução aos Arrays

---
+++ Módulo 6 - JavaScript + Logica/exemplos/18Arrays.js
@@ -0,0 +1,6 @@
+let listaNaves = ["Supernova", "Elemental", "Helmet"] // Padrão para elementos já definidos.
+let naveInfo = new Array("Colossus", 7, true) // Uso de new Array é para elementos ainda vazios. Ex.: new Array(10) (vetor com 10 elementos vazios)
+
+console.log("Primeiro elemento do array: " + listaNaves[0])
+console.log("Todos os elementos do array: " + listaNaves)
+console.log("Quantidade de tripulantes na nave " + naveInfo[0] + ": " + naveInfo[1] + " tripulantes.")
