f381e234529c9cec8a463e01f6715a9236fd0d96
Lucas Souza <lucassouzzamarques@gmail.com>
2021-10-20 12:10:47 -0300

    Comparações entre Objetos & Arrays

---
+++ Módulo 6 - JavaScript + Logica/exemplos/24ObjetoArray.js
@@ -0,0 +1,9 @@
+let spacialStation = {
+    name: "Receptora",
+    platformsQuantity: 15,
+    shape: "Esférica"
+}
+console.log(spacialStation.name.toUpperCase())
+
+let spaceshipNames = ["Fenix", "Puller", "Golias"]
+console.log(spaceshipNames[0].toUpperCase())
