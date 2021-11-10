d7662b078d8fcbde8b0e5e0e2e60e97a2d918c53
Lucas Souza <lucassouzzamarques@gmail.com>
2021-11-10 03:00:00 -0300

    Enviando o DOM para a função

---
+++ Módulo 7 - JavaScript + Logica (Intermediário)/aulas/16DOMFuncao.js
@@ -0,0 +1,14 @@
+function sayMyFirstName(element) {
+    alert("Meu primeiro nome é: " + element.value)
+}
+
+function sayMyLastName() {
+    console.log(event)
+    alert("Meu último nome é: " + event.target.value)
+}
+
+function show() {
+    let firstName = document.getElementById("first-name")
+    let lastName = document.getElementById("last-name")
+    alert("Nome Completo: " + firstName.value + " " + lastName.value)
+}
