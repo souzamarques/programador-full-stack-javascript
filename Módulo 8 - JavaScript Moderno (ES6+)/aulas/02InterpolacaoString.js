22db72603053791d036439fc09fab3dba71dbb2a
Lucas Souza <lucassouzzamarques@gmail.com>
2021-11-28 21:28:49 -0300

    Interpolação de String

---
+++ Módulo 8 - JavaScript Moderno (ES6+)/aulas/02InterpolacaoString.js
@@ -0,0 +1,9 @@
+let spaceshipName = "Colossus"
+console.log(`Nome da nave: ${spaceshipName}`)
+
+let newName = `Nome da nave novamente: ${spaceshipName}`
+console.log(newName)
+
+let velocity = 50
+console.log(`Velocidade da nave ${spaceshipName}: ${velocity}km/s`)
+console.log(`Velocidade da nave ${spaceshipName} triplicada: ${velocity * 3}km/s`)
