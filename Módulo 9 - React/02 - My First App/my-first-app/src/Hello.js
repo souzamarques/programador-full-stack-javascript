f42f3dbb2698fd67d3d6fbee3a9bcc48e8dfdd9e
Lucas Souza <lucassouzzamarques@gmail.com>
2022-01-12 19:49:23 -0300

    Criando o primeiro component

---
+++ Módulo 9 - React/02 - My First App/my-first-app/src/Hello.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+/* Componente de Classe
+class HelloWorld extends React.Component {
+    render() {
+        return <h1> Hello World </h1>;
+    }
+}
+*/
+
+// Componente de Função
+const HelloWorld = () => {
+    return <h1> Hello World </h1>;
+}
+
+export default HelloWorld;
