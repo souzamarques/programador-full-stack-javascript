11c0c3903b59f8f6588763d92182fb1181c2b7f0
Lucas Souza <lucassouzzamarques@gmail.com>
2022-01-12 21:47:44 -0300

    Resolução do Exercício: criando um component Lorem Ipsum

--- Módulo 9 - React/02 - My First App/my-first-app/src/App.js
+++ Módulo 9 - React/02 - My First App/my-first-app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import HelloWorld from './Hello';
+import LoremIpsum from './component/Lorem';
 
 function App() {
   return (
     <div>
       <HelloWorld />
+      <LoremIpsum />
     </div>
   );
 }
---
+++ Módulo 9 - React/02 - My First App/my-first-app/src/component/Lorem.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const LoremIpsum = () => {
+    return (
+        <p>
+            Suspendisse velit eros, elementum id congue euismod, molestie sit 
+            amet arcu. Etiam convallis vel nulla in auctor. Aenean non ante 
+            mollis, bibendum erat et, dapibus lectus. Nam sit amet cursus 
+            diam. Praesent varius tincidunt urna a semper. In gravida, mauris eu 
+            rutrum sodales, ex nibh mattis ex, fermentum convallis ligula metus 
+            non nisl. Sed a mauris et nisl rutrum tristique quis in purus. 
+            Sed in justo orci. Fusce vehicula erat a eros tristique dignissim. 
+            Cras vitae posuere nisl.
+        </p>
+    )
+}
+
+export default LoremIpsum;
